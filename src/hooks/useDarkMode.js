7ba10069c09f9b6e75240042f089f204c36af6c3
Karl Beksa <karlbeksa@gmail.com>
2019-07-16 16:50:57 -0400

    removed console.log

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -4,7 +4,6 @@ import {useLocalStorage} from './useLocalStorage';
 export const useDarkMode = () => {
     const [darkMode, setDarkMode] = useLocalStorage('dark', false);
     useEffect(() => {
-        console.log(darkMode)
         const body = document.getElementById('bodyTag');
         darkMode ? body.classList.add('dark-mode') : body.classList.remove('dark-mode');
 
